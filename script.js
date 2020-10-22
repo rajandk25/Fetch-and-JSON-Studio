e86744bc8e84d4beffd938393fdd0bbfbf5c0a68
rajandk25 <rajandk25@users.noreply.github.com>
2020-10-21 21:56:33 -0500

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+         response.json().then(function(json) {
+           //console.log(json);
+             const container = document.getElementById("container");
+             
+             container.innerHTML = `<h3>Number of Astronauts: ${json.length}</h3>`;
+
+             json.sort((a1, a2) => a2.hoursInSpace - a1.hoursInSpace);
+             
+             for(astronaut of json){
+                 let activeClass = '';
+                 if(astronaut.active === true) {
+                     activeClass = 'active';
+                 }
+                 
+                 container.innerHTML += `
+                 <div class="astronaut" id=${astronaut.id}>
+                 <div class="bio">
+                 
+                 <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                 <ul>
+                 <li>Hours in Space: ${astronaut.hoursInSpace}</li>
+                 <li><span class=${activeClass}>Active: ${astronaut.active}</span></li>
+                 <li>Skills: ${astronaut.skills.join(", ")}</li>
+                 </ul>
+                 </div>
+                 <img class="avatar" src="${astronaut.picture}">
+                 </div>
+                 
+                 `;
+                
+             }
+             
+        });
+});
+});
